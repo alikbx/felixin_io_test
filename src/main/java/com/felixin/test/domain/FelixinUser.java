1902fba8bc6ba403b639bc5e4d008ce62022d3f3
alireza rezaei <alikbx@gmail.com>
2021-09-24 17:28:55 +0330

    create domain

---
+++ src/main/java/com/felixin/test/domain/FelixinUser.java
@@ -0,0 +1,10 @@
+package com.felixin.test.domain;
+
+import com.felixin.test.security.AuthoritiesConstants;
+
+public final class FelixinUser {
+    public static final String name = "alireza";
+    public static final String user_id = "ali";
+    public static final String password = "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6";
+    public static final String role = AuthoritiesConstants.ADMIN;
+}
--- src/main/java/com/felixin/test/security/JwtUserDetailsService.java
+++ src/main/java/com/felixin/test/security/JwtUserDetailsService.java
@@ -3,20 +3,22 @@ package com.felixin.test.security;
 import java.util.ArrayList;
 import java.util.Collections;
 
+import com.felixin.test.domain.FelixinUser;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
 @Service
 public class JwtUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if ("alireza".equals(username)) {
-            return new User("alireza",
-                    "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
-                    Collections.singletonList(new SimpleGrantedAuthority(AuthoritiesConstants.ADMIN)));
+        if (FelixinUser.user_id.equals(username)) {
+            return new User(FelixinUser.user_id,
+                    FelixinUser.password,
+                    Collections.singletonList(new SimpleGrantedAuthority(FelixinUser.role)));
         } else {
             throw new UsernameNotFoundException("User not found with username: " + username);
         }
